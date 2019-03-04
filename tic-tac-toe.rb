ee11596b8e51ef18c2b0dc987fdcac4a81f58383
Carlos Sandoval <iznomag@gmail.com>
2019-03-04 10:40:44 -0500

    Add comments

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,6 +1,6 @@
-
 class Tic_tac_toe
   
+  ##Vamos a trabajar en base a matrices y arreglos
   def initialize
     @p1_array = [""]*9
     @p2_array = [""]*9
@@ -10,23 +10,22 @@ class Tic_tac_toe
   end
  
  
-
+  ##Metodo para recibir el valor del player 1 
   def get_p1
     show = @show_array.each_index {|val| val}
-    showCortadito = show.each_slice(3).to_a
+    showCortadito = show.each_slice(3).to_a #Dividimos el arreglo en 3 partes para formar el tic-tac-toe
     puts "Ingrese opcion player1:"
-    puts "#{showCortadito[0]}\n#{showCortadito[1]}\n#{showCortadito[2]}"
-  
-    
-    @player1_index = gets.chomp.to_i   
+    puts "#{showCortadito[0]}\n#{showCortadito[1]}\n#{showCortadito[2]}" #Mostramos las jugadas
+     @player1_index = gets.chomp.to_i   
     return @player1_index
   end
 
+  ##Metodo para recibir el valor del player 2
   def get_p2
     show = @show_array.each_index {|val| val}
-    showCortadito = show.each_slice(3).to_a
+    showCortadito = show.each_slice(3).to_a #Dividimos el arreglo en 3 partes para formar el tic-tac-toe
     puts "Ingrese opcion player2:"
-    puts "#{showCortadito[0]}\n#{showCortadito[1]}\n#{showCortadito[2]}"
+    puts "#{showCortadito[0]}\n#{showCortadito[1]}\n#{showCortadito[2]}" #Mostramos las jugadas
     @player2_index = gets.chomp.to_i   
     return @player2_index
   end
@@ -34,21 +33,23 @@ class Tic_tac_toe
 
 
   def set_player1       
-    @p1_array[@player1_index]  = "X" 
+    @p1_array[@player1_index]  = "X" #El efecto visual del player-1 en el tic tac toe 
     @show_array[@player1_index] = "X"
   end
 
   def set_player2
-    @p2_array[@player2_index] = "O"
+    @p2_array[@player2_index] = "O" #El efecto visual del player-2 en el tic tac toe
     @show_array[@player2_index] = "O"   
   end
   
+  ##Metodo para determinar el ganador
   def set_winner
     winner = false
     steps = 0
     
     while winner == false && steps <= 4 do
       
+      ##Recibimos y asignamos el input del player-1
       get_p1
       set_player1
       
@@ -67,11 +68,13 @@ class Tic_tac_toe
       (!@p1_array[3].empty? && !@p1_array[4].empty? && !@p1_array[5].empty?) ? (winner = true;puts "Player 1 Won";break) : winner = false
 
 
+      ##Recibimos y asignamos el input del player-2
       if winner === false && steps < 4
         get_p2
         set_player2 
       end
       #Player 2 possibilities of win
+      #Borders
       (!@p2_array[2].empty? && !@p2_array[5].empty? && !@p2_array[8].empty?) ? (winner = true;puts "Player 2 Won";break) : winner = false
       (!@p2_array[6].empty? && !@p2_array[7].empty? && !@p2_array[8].empty?) ? (winner = true;puts "Player 2 Won";break) : winner = false
       (!@p2_array[0].empty? && !@p2_array[3].empty? && !@p2_array[6].empty?) ? (winner = true;puts "Player 2 Won";break) : winner = false
@@ -85,7 +88,7 @@ class Tic_tac_toe
     
     steps += 1
     end
-    
+    ##Condicional para determinar el empate
     if winner == false
       puts "DRAW"
     end
